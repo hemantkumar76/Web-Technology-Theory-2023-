4d8874e99e2fb6af28cc293a486f7001bdd36123
Hemant <92645502+hemantkumar76@users.noreply.github.com>
2023-02-07 22:45:38 +0530

    Create _1.java

---
+++ Theory 2/_1.java
@@ -0,0 +1,21 @@
+import java.util.*;
+class _1
+{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter a number");
+        int n=sc.nextInt();
+        int a=n%10;
+        int b=(n/10)%10;
+        int c=n/100;
+        if(a==c)
+        {
+            System.out.println("palindrome");
+        }
+        else
+        {
+            System.out.println("palindrome");
+        }
+    }
+}
