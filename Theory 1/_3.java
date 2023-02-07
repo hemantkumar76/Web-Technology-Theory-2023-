762005d81842377b590f5e35392b26792e82f352
Hemant <92645502+hemantkumar76@users.noreply.github.com>
2023-02-07 22:43:38 +0530

    Create _3.java

---
+++ Theory 1/_3.java
@@ -0,0 +1,12 @@
+class _3 {
+    public static void main(String[] args)
+    {
+        int num = 123;
+        int a = num/100;
+        int b = (num%100)/10;
+        int c = num%10;
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+    }
+}
